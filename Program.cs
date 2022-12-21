1845fac3f9012a1113c93cd29c5fcaee5a9a25a5
mohamed36363 <112127545+mohamed36363@users.noreply.github.com>
2022-12-21 14:21:52 +0200

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,31 @@
+﻿namespace test
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int startNumber, endNumber;
+
+            Console.Write("Enter Start Number: ");
+            startNumber = int.Parse(Console.ReadLine());
+
+            Console.Write("Enter End Number: ");
+            endNumber = int.Parse(Console.ReadLine());
+
+            Console.WriteLine("the perfect number between " + startNumber + " and " + endNumber + "is: ");
+
+            for (int i = startNumber; i <= endNumber; i++)
+            {
+                int sum = 0;
+                for (int j = 1; j < i; j++)
+                {
+                    if (i % j == 0)
+                        sum = sum + j;
+                }
+                if (sum == i)
+                    Console.WriteLine("\t" + i);
+            }
+            Console.ReadKey();
+        }
+    }
+}
